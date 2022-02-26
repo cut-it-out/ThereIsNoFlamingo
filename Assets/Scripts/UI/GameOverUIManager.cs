1cde53fdb51a6253e450d21b7049298693c7b8e1
cut-it-out <43676292+cut-it-out@users.noreply.github.com>
2022-02-27 00:19:12 +0100

    update UI and some finetunings
    add new font

--- Assets/Scripts/UI/GameOverUIManager.cs
+++ Assets/Scripts/UI/GameOverUIManager.cs
@@ -4,12 +4,15 @@ using TMPro;
 public class GameOverUIManager : MonoBehaviour
 {
     [SerializeField] TMP_Text highScore;
+    [SerializeField] TMP_Text levelReached;
 
     private void OnEnable()
     {
         int maxScoreReached = Game.GetInstance().CurrentScore;
+        float maxLevelReached = Spawner.GetInstance().GetProgressMultiplier();
         //maxScoreReached = maxScoreReached == 0 ? 1 : maxScoreReached;
         highScore.text = maxScoreReached.ToString();
+        levelReached.text = "LEVEL REACHED: " + maxLevelReached.ToString();
     }
     
 }
